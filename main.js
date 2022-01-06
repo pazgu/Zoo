ac815995c8eab1929a6d0868b4e0fdb2ada0e411
pazgu <96666883+pazgu@users.noreply.github.com>
2022-01-06 14:43:02 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+
+class zoo {
+    ZooName = `Biblical Zoo`;
+    OpeningHours = '8 am till 9 pm';
+    HasResturants = true;
+    PlaceAt = "Jerusalem";
+    AgeOfTheVisitor;
+
+    HowManyVisitors(){
+        let visitors=0;
+        while (visitors!=7000){
+            visitors+=1;
+             let message="You can still enter + ${7000-visitors} + people to the zoo";
+        }
+         message= "The place is full";
+        return message;
+    }
+
+    TicketPrice(AgeOfTheVisitor){
+        AgeOfTheVisitor=this.AgeOfTheVisitor;
+        if (AgeOfTheVisitor<=5){
+            console.log("The price for the ticket for child under the age 6 is free");
+        }
+        else if(AgeOfTheVisitor>5 && AgeOfTheVisitor<60) {
+            console.log("The price for adult is 50 shekels");
+        }
+        else{
+            console.log("The price for retriee is 35 shekels");
+        }
+        }
+    }
+
+
+
+
